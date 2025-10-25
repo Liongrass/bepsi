3639f64404cf696728fb1f1b7ef4f16fb37d49d3
Leo <2104935+Liongrass@users.noreply.github.com>
2025-10-24 21:18:43 -0700

    add functionality that lets us detect which trays are available

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const { createExitAwareAbortController } = require("./src/common");
+const { setupButtons } = require("./src/machine");
+const { checkTrays } = require("./src/machine");
 const { startButtonListener } = require("./src/machine");
 const { startLightningListener } = require("./src/listeners/lightningL");
+
 
 const main = async () => {
   console.log(`Starting 21UP`);
   const abortController = createExitAwareAbortController();
+  setupButtons();
   startButtonListener();
+  checkTrays();
   startLightningListener();
 };
 
--- src/machine.js
+++ src/machine.js
@@ -9,22 +9,52 @@ const pinIn = JSON.parse(process.env.PIN_IN);
 const pinOut = JSON.parse(process.env.PIN_OUT);
 const label = JSON.parse(process.env.LABEL);
 
+const availableTray = [null, null, null, null, null, null];
+const button = [];
 let state = 0;
 const stateLabel = ["cold", "hot"]
+const fullLabel = ["full", "empty"]
 var timetaken = "Time until button was pressed:";
 
 // A function that always listens to buttons /
 // Logs which button was pressed
 
-const startButtonListener = async () => {
+function setupButtons() {
+//  let button = [];
 for (let pin of pinIn) {
-    const button = new Gpio(pin, "in", "falling", {debounceTimeout: 500});
-    console.log(`Waiting for button to be pressed. Pin ${pinIn[pinIn.indexOf(pin)]}`);
+    button[pin] = new Gpio(pin, "in", "falling", {debounceTimeout: 500});
+    console.log(`Waiting for button to be pressed. Pin ${pin}`);
+  }
+}
+
+const startButtonListener = async () => {
     // Listens for a signal on button, check for the .env file for the pin numbers
-    button.watch((error, value) => {
+  for (let pin of pinIn) {
+    button[pin].watch((error, value) => {
       tray = pinIn.indexOf(pin);
       console.log(`Button press detected. Tray ${tray} at pin ${pin}. (${label[tray]})`);
       makeMachineCold(pinOut);
+      if (error) {
+        throw error;
+      }
+    })
+  }
+}
+
+// The output is suppoed to be an array where 0 represents unavailable and 1 represents available
+// availableTray = [0, 0, 1, 0, 1, 1]
+
+function checkTrays() {
+  for (let pin of pinIn) {
+    button[pin].read((error, value) => {
+// Use the splice function to assemble the array always in the correct order. splice(position, numberOfItemsToRemove, item)
+      availableTray.splice(pinIn.indexOf(pin), 1, value);
+//      console.log(`Tray ${pinIn.indexOf(pin)} Value ${value}`)
+//      console.log(`Tray ${pinIn.indexOf(pin)} at ${pin} is ${fullLabel[value]} (${label[pinIn.indexOf(pin)]})`);
+      console.log(`Available trays: ${availableTray}`);
+      if (error) {
+        throw error;
+      }
     })
   }
 }
@@ -34,9 +64,11 @@ for (let pin of pinIn) {
 // Logs that the circuit was opened /
 
 function makeMachineHot(pinOut) {
+  console.log(`Checking available trays: ${availableTray}`);
   console.time(timetaken);
   const pino = new Gpio(pinOut, "out");
-  pino.writeSync(1);
+// On the test relay the pins are inverted, in production they should be 1 to open
+  pino.writeSync(0);
   state = 1;
   console.log(`Machine is now ${stateLabel[state]}. Pin ${pinOut}`);
 }
@@ -46,17 +78,25 @@ function makeMachineHot(pinOut) {
 // If variable is False, do nothing
 
 async function makeMachineCold(pinOut) {
+// First, the code sleeps for half a second to allow the machine to dispense the pop
   await sleep(500);
-
-  if (state == 1) {
-  console.timeEnd(timetaken)
-  console.log(`Dispensed ${label[tray]} at pin ${pinOut} successfully`);
-  const pino = new Gpio(pinOut, "out");
-  pino.writeSync(0);
-  state = 0;
-  console.log(`Machine is now ${stateLabel[state]}.`)
+// Next, the machine will check whether the tray was previously marked unavailable
+  if (availableTray[tray] == 1) {
+    console.log(`Item ${label[tray]} at tray ${tray} unavailable`)
+    return;
   }
-
+// Third, the machine will check if it is actually hot, and only close the circuit then
+  else if (state == 1) {
+    console.timeEnd(timetaken)
+    console.log(`Dispensed ${label[tray]} at pin ${pinOut} successfully`);
+    const pino = new Gpio(pinOut, "out");
+// On the test relay the pins are inverted, in production they should be 0 to close
+    pino.writeSync(1);
+    state = 0;
+    console.log(`Machine is now ${stateLabel[state]}.`)
+    checkTrays();
+  }
+// If condition 0 & 1 are not met, it will do nothing.
   else {
   console.log(`Machine is already ${stateLabel[state]}. Doing nothing.`)
   }
@@ -64,6 +104,8 @@ async function makeMachineCold(pinOut) {
 
 
 module.exports = {
+  setupButtons,
+  checkTrays,
   startButtonListener,
   makeMachineHot,
   pinOut,
